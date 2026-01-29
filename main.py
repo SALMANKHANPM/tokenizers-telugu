eda3f5b676edb4b4b0a36cf35162abc5dd1ca1ac
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2026-01-29 18:05:28 +0530

    init : tokenizers usage

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+from pathlib import Path
+from src.data.models import model_id
+from typing import List
+
+
+class TokenizerFactory:
+    @classmethod
+    def get_tokenizer(cls, model_name: str):
+        for model, model_path, tokenizer_class in model_id:
+            if model_name == model:
+                print("Loading tokenizer for model: ", model_name)
+                print("Model path: ", model_path)
+                print("Tokenizer class: ", tokenizer_class.__name__)
+                return tokenizer_class.get_instance(model_path)
+        raise ValueError(f"Tokenizer not found for model: {model_name}")
+    
+    def __init__(self, model_name: str):
+        self.tokenizer = self.get_tokenizer(model_name)
+        
+    def encode(self, text: str):
+        return self.tokenizer.encode(text)
+    
+    def decode(self, token_ids: List[int]):
+        return self.tokenizer.decode(token_ids)
+    
+    
+    @classmethod
+    def get_all_tokenizers(cls):
+        return [tokenizer_class.get_instance(model_path) for model, model_path, tokenizer_class in model_id]
+    
+    def save_vocabulary(self):
+        with open(self.save_path, "w") as f:
+            json.dump(self.tokenizer.vocab, f, indent=2, ensure_ascii=False)
+        
+    
+if __name__ == "__main__":
+    pass
+    # Usage
+    # tokenizer = TokenizerFactory.get_tokenizer(model_name="llama3")
+    # tokenizer.save_vocabulary()
+    # tokens = tokenizer.encode("ఎలా టైపు చెయ్యాలో వివరంగా తెలుసుకోండి, Hello, how are you?")
+    # for token in tokens:
+    #     print(tokenizer.decode([token]), token)
